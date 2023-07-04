b6943f60d805af87cab34b26b9c4252009cdb47e
Murilo Gabriel <68136262+muriloizi@users.noreply.github.com>
2023-07-04 20:49:58 -0300

    Add files via upload

---
+++ importandoBib.py
@@ -0,0 +1,9 @@
+#importando nossa biblioteca
+import primeraBiblioteca as bib
+##from primeraBiblioteca import buscarNoTelefone
+
+#chamando uma funcao ao nossa biblioteca
+nome = input("Qual contato você está buscando? ")
+bib.buscarNoTelefone(nome)
+##buscarNoTelefone(nome)
+#mostrando o resultado
